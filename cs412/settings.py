50157a1da344de11313560ea9ba72172a9251197
jchan042 <jchan042@bu.edu>
2026-01-29 12:04:09 -0500

    settings

--- cs412/settings.py
+++ cs412/settings.py
@@ -140,6 +140,5 @@ import socket
 CS_DEPLOYMENT_HOSTNAME = 'cs-webapps.bu.edu'
 
 if socket.gethostname() == CS_DEPLOYMENT_HOSTNAME:
-    print("Inside gethostname")
     STATIC_URL = '/jchan042/static/'
     MEDIA_URL = '/jchan042/media/'
