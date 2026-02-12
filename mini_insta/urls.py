cdc5a01983ba0229a7e841ccdf89406c904d31b1
jchan042 <jchan042@bu.edu>
2026-02-12 16:58:52 -0500

    Finished single profile html

--- mini_insta/urls.py
+++ mini_insta/urls.py
@@ -3,9 +3,11 @@
 # Description: Define which views to display for which URLs
 
 from django.urls import path
-from .views import ProfileListView  # hardcoding importing all the views 
+from .views import ProfileListView, ProfileDetailView  # hardcoding importing all the views 
 
+# Routes URLs to display their respective view
 urlpatterns = [
     path('', ProfileListView.as_view(), name="show_all_profiles"),
     path('show_all_profiles', ProfileListView.as_view(), name="show_all_profiles"),
+    path('profile/<int:pk>', ProfileDetailView.as_view(), name="profile"),
 ]
--- mini_insta/views.py
+++ mini_insta/views.py
@@ -3,13 +3,23 @@
 # Description: Returns an HTML template view for each URL 
 
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Profile
 
 # Create your views here.
+
+# View for all profiles
 class ProfileListView(ListView):
     '''Define a view class to show all Instagram Profiles'''
     
     model = Profile
     template_name = "mini_insta/show_all_profiles.html"  # fixed path
     context_object_name = "profiles"  # plural
+    
+# View for one single profile 
+class ProfileDetailView(DetailView):
+    '''Define a view class to show a single profile'''
+    
+    model = Profile 
+    template_name = "mini_insta/show_profile.html"
+    context_object_name = "profile" # singular 
